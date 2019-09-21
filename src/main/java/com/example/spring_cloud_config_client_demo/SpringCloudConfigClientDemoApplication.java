13d09e1728c9cbf9f714f1f8c391984fc5b00652
chen_anguo <cag2050@163.com>
2019-09-21 12:04:00 +0800

    project init

---
+++ src/main/java/com/example/spring_cloud_config_client_demo/SpringCloudConfigClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_config_client_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
+    }
+
+}
